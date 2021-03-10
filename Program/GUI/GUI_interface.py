c8f2ce438671fe97ee7c7adc8e83df9e45dd8d0c
Markfesenk0 <markfesenk0@gmail.com>
2021-03-10 11:36:37 +0200

    Added new calculations

--- Program/GUI/GUI_interface.py
+++ Program/GUI/GUI_interface.py
@@ -1,17 +1,20 @@
+import pathlib
+import pickle
+import threading
 from tkinter import Tk, ttk, filedialog, Label, Entry, Button, Checkbutton, BooleanVar, Canvas, W, E, StringVar
 
 from Calculations.Calculations import CalculateAngles, CalculateMeasurement
 from Processing.Sync import SyncByMovementOpenpose, SyncByMovementVicon, matchTimestamps
 from Readers.BagFile import BagFileSetup
 from Readers.Vicon import ViconReader
-from Utilities.GraphGenerator import GenerateGraph
+from Utilities.GraphGenerator import GenerateGraph, GenerateGraphOfCorrelation
 
 
 class GuiInterface(object):
     def __init__(self):
         self.title = "Image Processing Project"
-        # self.path = "C:\\Users\\markf\\Downloads\\Project\\RunMe\\"
-        self.path = "C:\\Age_Estimation_Project\\bag_files\sub003\\Squat\\"
+        self.path = "C:\\Users\\markf\\Downloads\\Project\\sub003\\Squat\\"
+        # self.path = "C:\\Age_Estimation_Project\\bag_files\sub003\\Squat\\"
         self.root = None
         self.combo = None
         self.textBox = None
@@ -23,7 +26,7 @@ class GuiInterface(object):
         self.sideCheckBox = None
         self.backCheckBox = None
         self.orientations = []
-        self.calculation = []
+        self.calculations = []
 
     def setupInterface(self):
         # initialize windows
@@ -32,20 +35,8 @@ class GuiInterface(object):
         self.root.configure(bd=10)
         # self.root.geometry("300x200")
 
-        # # add label and choises of algorithms
-        # Label(self.root, text="Algorithm:").grid(row=0, column=0, sticky=W)
-        # self.combo = ttk.Combobox(self.root, width=10, values=self.algorithms)
-        # self.combo.current(4)
-        # self.combo.grid(row=0, column=1, sticky=W)
-
-        # # add label and time limit entry
-        # Label(self.root, text="Time Limit").grid(row=1, column=0, sticky=W)
-        # self.runtimeEntry = Entry(self.root, width=13)
-        # self.runtimeEntry.grid(row=1, column=1, sticky=W)
-        # self.runtimeEntry.insert(0, "100")
-
         # Calculation checkboxes:
-        Label(self.root, text="Calculations").grid(row=0, column=0, sticky=W, pady=5)
+        Label(self.root, text="Calculations").grid(row=0, column=0, sticky=W + E, pady=5)
         # add kyphosis button
         self.KyphosisCheckBox = BooleanVar()
         self.KyphosisCheckBox.set(False)
@@ -66,20 +57,20 @@ class GuiInterface(object):
                                                                                           sticky=W)
 
         # Orientation checkboxes:
-        Label(self.root, text="Orientations").grid(row=0, column=1, sticky=E, pady=5)
-        # add front button
+        Label(self.root, text="Orientations").grid(row=0, column=1, sticky=W + E, pady=5)
+        # add front camera button
         self.frontCheckBox = BooleanVar()
         self.frontCheckBox.set(False)
         Checkbutton(self.root, text="Front", variable=self.frontCheckBox,
                     command=lambda: self.addToOrientation('Front'), cursor="hand2").grid(row=1, column=1,
                                                                                          sticky=W)
-        # add side center button
+        # add side camera button
         self.sideCheckBox = BooleanVar()
         self.sideCheckBox.set(False)
         Checkbutton(self.root, text="Side", variable=self.sideCheckBox,
                     command=lambda: self.addToOrientation('Side'), cursor="hand2").grid(row=2, column=1,
                                                                                         sticky=W)
-        # add back button
+        # add back camera button
         self.backCheckBox = BooleanVar()
         self.backCheckBox.set(False)
         Checkbutton(self.root, text="Back", variable=self.backCheckBox,
@@ -88,47 +79,44 @@ class GuiInterface(object):
 
         # add choose file button
         Button(self.root, text="Vicon Path", width=16, command=lambda: self.browseFile(), cursor="hand2",
-               activebackground="Lavender").grid(row=4, column=0, sticky=W + E, padx=10, pady=10)
+               activebackground="Lavender").grid(row=4, column=0, sticky=W + E, padx=15, pady=10)
 
-        # add run button
-        Label(self.root, textvariable=self.textBox).grid(row=1, column=2, sticky=W + E, columnspan=4)
-        button_run = Button(self.root, text="Run", command=lambda: self.runButton(), cursor="hand2",
-                            activebackground="Lavender").grid(row=4, column=1, sticky=W + E, padx=10, pady=10)
-
+        # add text box
         self.textBox = StringVar()
         self.textBox.set("Welcome")
-
+        Label(self.root, textvariable=self.textBox).grid(row=5, column=0, sticky=W + E, columnspan=4)
+        # add run button
+        button_run = Button(self.root, text="Run", command=lambda: self.startMainThread(), cursor="hand2",
+                            activebackground="Lavender", width=10).grid(row=4, column=1, sticky=W + E, padx=15, pady=10)
         self.root.mainloop()
 
     def runButton(self):
-        self.Main(self.path, self.orientations, self.calculation)
-        # self.mazeScreen.clear()
-        # maxRunTime = int(self.runtimeEntry.get())
-        # algorithmName, startNode, goalNode, mazeSize, maze = Utilities.readInstance(self.path)
-        # algorithm, isHeuristic = self.getAlgorithmFromString(self.combo.get(), self.visualCheckBox.get())
-        self.textBox.set(", please wait...")
-        # self.mazeScreen.update()
-        # if isHeuristic is True:
-        #     algorithm(maze, maxRunTime, "minimumMoves")
-        #     # algorithm(maze, maxRunTime, "movesCount")
-        # else:
-        #     algorithm(maze, maxRunTime)
-        self.textBox.set("Finished running. See OutputResult.txt for results")
-        # self.mazeScreen.update()
+        if len(self.orientations) == 0:
+            self.textBox.set("Please choose camera orientation settings")
+        elif len(self.calculations) == 0:
+            self.textBox.set("Please choose measurements")
+        else:
+            self.textBox.set("Working...")
+            self.Main()
+            self.textBox.set("Done!")
+
+    def startMainThread(self):
+        global funcThread
+        funcThread = threading.Thread(target=self.runButton)
+        funcThread.start()
 
     def browseFile(self):
         file = filedialog.askopenfile(parent=self.root, mode='rb', title='Choose a file')
-        # algorithmName, startNode, goalNode, mazeSize, maze = Utilities.readInstance(file.name)
-        # for item in self.algorithms:
-        #     if algorithmName.lower() == item.lower():
-        #         self.combo.current(self.algorithms.index(item))
-        self.path = file.name
+        try:
+            self.path = file.name.replace('vicon.csv', '')
+        except:
+            self.textBox.set("Please select a valid path")
 
     def addToCalculations(self, param):
-        if param in self.calculation:
-            self.calculation.remove(param)
+        if param in self.calculations:
+            self.calculations.remove(param)
             return
-        self.calculation.append(param)
+        self.calculations.append(param)
 
     def addToOrientation(self, param):
         if param in self.orientations:
@@ -136,28 +124,34 @@ class GuiInterface(object):
             return
         self.orientations.append(param)
 
-    def Main(self, path, orientations, calculations):
+    def Main(self):
         openposeSkeletonsLists = []
         openposeTimestampsLists = []
 
         # List of vicon skeletons
-        viconSkeletons = ViconReader(path + 'vicon.csv')
+        viconSkeletons = ViconReader(self.path + 'vicon.csv')
         viconSkeletons = SyncByMovementVicon(viconSkeletons)
         # Two lists: one of Openpose skeletons, one of timestamps
-        for i in range(len(orientations)):
-            print(orientations[i])
-            openposeSkeletons, openposeTimestamps = BagFileSetup(path, orientations[i])
+        for i in range(len(self.orientations)):
+            # print(self.orientations[i])
+            self.textBox.set("Working on " + str(i + 1) + '/' + str(len(self.orientations)) + " bag file")
+            openposeSkeletons, openposeTimestamps = BagFileSetup(self.path, self.orientations[i])
             openposeSkeletons, openposeTimestamps = SyncByMovementOpenpose(openposeSkeletons, openposeTimestamps)
             openposeSkeletonsLists.append(openposeSkeletons)
             openposeTimestampsLists.append(openposeTimestamps)
 
+        # pickle.dump(openposeSkeletonsLists, open(self.path + 'loadfiles\\' + "openposeSkeletonsLists", 'wb'))
+        # pickle.dump(openposeTimestampsLists, open(self.path + 'loadfiles\\' + "openposeTimestampsLists", 'wb'))
+        # openposeSkeletonsLists = pickle.load(open(self.path + 'loadfiles\\' + "openposeSkeletonsLists", 'rb'))
+        # openposeTimestampsLists = pickle.load(open(self.path + 'loadfiles\\' + "openposeTimestampsLists", 'rb'))
+
+        self.textBox.set("Calculating measurements...")
         openposeMeasurementsMat = []
         viconMeasurementsMat = []
-        for i in range(len(calculations)):
-            # openposeTimestampsLists.append(CalculateAnglesOpenpose())
+        for i in range(len(self.calculations)):
             openposeMeasurements = []
             viconMeasurements = []
-            for j in range(len(orientations)):
+            for j in range(len(self.orientations)):
                 openposeData, correspondingTimestamps = CalculateMeasurement(openposeSkeletonsLists[j], calculations[i], openposeTimestampsLists[j])
                 openposeMeasurements.append([openposeData, correspondingTimestamps])
                 viconData = CalculateMeasurement(viconSkeletons, calculations[i])
@@ -167,7 +161,12 @@ class GuiInterface(object):
             openposeMeasurementsMat.append(openposeMeasurements)
             viconMeasurementsMat.append(viconMeasurements)
 
-        for i in range(len(calculations)):
-            for j in range(len(orientations)):
-                print('Graph of ' + calculations[i] + ' from ' + orientations[j] + ' Camera')
-                GenerateGraph(openposeMeasurementsMat[i][j][0], openposeMeasurementsMat[i][j][1], viconMeasurementsMat[i][j], calculations[i], orientations[j],"C:\Age_Estimation_Project\\bag_files")
+        self.textBox.set("Exporting graphs...")
+        graphsOutput = self.path + 'Graphs\\'
+        pathlib.Path(graphsOutput).mkdir(parents=True, exist_ok=True)
+        for i in range(len(self.calculations)):
+            for j in range(len(self.orientations)):
+                GenerateGraph(openposeMeasurementsMat[i][j][1], openposeMeasurementsMat[i][j][0],
+                              viconMeasurementsMat[i][j], self.calculations[i], self.orientations[j], graphsOutput)
+                GenerateGraphOfCorrelation(openposeMeasurementsMat[i][j][0], viconMeasurementsMat[i][j],
+                                           self.calculations[i] + '_' + self.orientations[j], graphsOutput)
